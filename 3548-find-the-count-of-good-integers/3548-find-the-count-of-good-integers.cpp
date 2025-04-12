ac9aa7b9b8527293d9e311b91f7cbb2ce101bd05
Rakesh Kumar <75713012+aarvi18@users.noreply.github.com>
2025-04-12 17:44:06 +0530

    Time: 514 ms (51.73%), Space: 23.4 MB (68.97%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
@@ -1,100 +1,45 @@
 class Solution {
-public:
-    long long factorialArr[11];
-    
-    Solution() {
-        factorialArr[0] = 1;
-        for(int idx = 1; idx <= 10; idx++) {
-            factorialArr[idx] = factorialArr[idx - 1] * idx;
-        }
+ public:
+  long long countGoodIntegers(int n, int k) {
+    const int halfLength = (n + 1) / 2;
+    const int minHalf = pow(10, halfLength - 1);
+    const int maxHalf = pow(10, halfLength);
+    long ans = 0;
+    unordered_set<string> seen;
+
+    for (int num = minHalf; num < maxHalf; ++num) {
+      const string firstHalf = to_string(num);
+      const string secondHalf = {firstHalf.rbegin(), firstHalf.rend()};
+      const string palindrome = firstHalf + secondHalf.substr(n % 2);
+      if (stol(palindrome) % k != 0)
+        continue;
+      string sortedDigits = palindrome;
+      ranges::sort(sortedDigits);
+      if (seen.contains(sortedDigits))
+        continue;
+      seen.insert(sortedDigits);
+      vector<int> digitCount(10);
+      for (const char c : palindrome)
+        ++digitCount[c - '0'];
+      // Leading zeros are not allowed, so the first digit is special.
+      const int firstDigitChoices = n - digitCount[0];
+      long permutations = firstDigitChoices * factorial(n - 1);
+      // For each repeated digit, divide by the factorial of the frequency since
+      // permutations that swap identical digits don't create a new number.
+      for (const int freq : digitCount)
+        if (freq > 1)
+          permutations /= factorial(freq);
+      ans += permutations;
     }
 
-    vector<long long> generatePalindromes(int digitCount) {
-        vector<long long> palindromeList;
-        if(digitCount == 0) return palindromeList;
-        
-        int halfLen = (digitCount + 1) / 2;
-        long long startNum = pow(10, halfLen - 1);
-        long long endNum = pow(10, halfLen) - 1;
-        
-        if(digitCount == 1) startNum = 0;
-        
-        for(long long firstHalf = startNum; firstHalf <= endNum; firstHalf++) {
-            string halfStr = to_string(firstHalf);
-            string fullPalindrome;
+    return ans;
+  }
 
-            if(digitCount % 2 == 0) {
-                string reversedHalf = halfStr;
-                reverse(reversedHalf.begin(), reversedHalf.end());
-                fullPalindrome = halfStr + reversedHalf;
-            } else {
-                string truncatedHalf = halfStr.substr(0, halfStr.size() - 1);
-                reverse(truncatedHalf.begin(), truncatedHalf.end());
-                fullPalindrome = halfStr + truncatedHalf;
-            }
-
-            if(fullPalindrome.size() == digitCount) {
-                long long palindromeNum = stoll(fullPalindrome);
-                palindromeList.push_back(palindromeNum);
-            }
-        }
-        return palindromeList;
-    }
-
-    vector<int> countDigits(long long number, int digitCount) {
-        vector<int> digitFreq(10, 0);
-        string numStr = to_string(number);
-        while(numStr.size() < digitCount) numStr = "0" + numStr;
-        
-        for(char digit : numStr) digitFreq[digit - '0']++;
-        return digitFreq;
-    }
-
-    long long calculatePermutations(vector<int> digitFreq, int digitCount) {
-        long long permutationCount = 0;
-        for(int digit = 1; digit <= 9; digit++) {
-            if(digitFreq[digit] == 0) continue;
-            
-            vector<int> adjustedFreq = digitFreq;
-            adjustedFreq[digit]--;
-            bool isValid = true;
-            
-            for(int freq : adjustedFreq) {
-                if(freq < 0) {
-                    isValid = false;
-                    break;
-                }
-            }
-            
-            if(!isValid) continue;
-            
-            long long validPermutations = factorialArr[digitCount - 1];
-            for(int idx = 0; idx < 10; idx++) {
-                validPermutations /= factorialArr[adjustedFreq[idx]];
-            }
-            permutationCount += validPermutations;
-        }
-        return permutationCount;
-    }
-
-    long long countGoodIntegers(int digitCount, int divisor) {
-        vector<long long> palindromeList = generatePalindromes(digitCount);
-        vector<long long> validPalindromes;
-        
-        for(auto palindrome : palindromeList) {
-            if(palindrome % divisor == 0) validPalindromes.push_back(palindrome);
-        }
-        
-        set<vector<int>> uniqueDigitFreq;
-        for(auto validPalindrome : validPalindromes) {
-            vector<int> digitFreq = countDigits(validPalindrome, digitCount);
-            uniqueDigitFreq.insert(digitFreq);
-        }
-        
-        long long totalGoodIntegers = 0;
-        for(auto &digitFreq : uniqueDigitFreq) {
-            totalGoodIntegers += calculatePermutations(digitFreq, digitCount);
-        }
-        return totalGoodIntegers;
-    }
+ private:
+  long factorial(int n) {
+    long res = 1;
+    for (int i = 2; i <= n; ++i)
+      res *= i;
+    return res;
+  }
 };
